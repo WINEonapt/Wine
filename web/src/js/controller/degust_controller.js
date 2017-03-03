d0cbfca785a7d60501bf1951af32c2f77198f7ef
orionivv <orionivvlad@gmail.com>
2017-03-03 08:06:56 +0200

    new menu

--- web/src/js/controller/degust_controller.js
+++ web/src/js/controller/degust_controller.js
@@ -9,6 +9,9 @@ angular.module("app")
         $scope.rad =[];
         var r = $scope.rad;
 
+        $scope.people = [];
+        var people = $scope.people;
+        // people = 2;
         // r.vin =10;
         // r.cvet = 0;
         // r.trav = 0;
@@ -24,39 +27,63 @@ angular.module("app")
         // r.slad = 0;
         // r.tip = 0;
         // r.dlit = 0;
-       
+        $scope.show=1;
 
-        
     $scope.goFuck = ()=> {
     // for ( var key in r) {
             // console.log("Ключ: " + key + " значение: " + r[key])
         // }
+        // var g = $scope.graf.value;
+        console.log($scope.show)
     }
 
 
-// GRAFIK-start
-     $scope.drawGrafik = ()=> {
-        $scope.labels =["Винный аромат", "Цветочный аромат", "Окисленный", "Интенсивность вкуса", "Кислотный вкус", "Типичность вкуса", "Длительность вкуса"];
+// GRAFIK-start//////////////////////////////////////////////////////////////////
+        $scope.grafik = [];
+        var g = $scope.grafik;
+        g.g =1;
 
-        $scope.data = [
+        // if (g.g==1) {
+        //     $("#green").css({
+        //         background: "red"
+        //     })
+        // }
+
+     $scope.drawGrafik = ()=> {
+        $scope.grafik = [];
+        var g = $scope.grafik;
+        g.g =1;
+
+        $scope.labelsRadar =["Винный аромат", "Цветочный аромат", "Окисленный", "Интенсивность вкуса", "Кислотный вкус", "Типичность вкуса", "Длительность вкуса"];
+        $scope.dataRadar = [
         [r.vin, r.cvet, r.okis, r.intens, r.kislot, r.tip, r.dlit],
         [0,0,0,0,0,10]
         ];
-  }
-//GRAFIK-end
 
-//RANDOM-start
+        $scope.labelsChart =["Винный аромат", "Цветочный аромат", "Окисленный", "Интенсивность вкуса", "Кислотный вкус", "Типичность вкуса", "Длительность вкуса"];
+        $scope.dataChart = [
+        [r.vin, r.cvet, r.okis, r.intens, r.kislot, r.tip, r.dlit],
+        [0,0,0,0,0,0]
+        ];
+
+        $scope.labelsArea =["Винный аромат", "Цветочный аромат", "Окисленный"];
+        $scope.dataArea = [43,23,42];
+
+  }
+//GRAFIK-end/////////////////////////////////////////////////////////////////
+
+//RANDOM-start/////////////////////////////////////////////////////////////////
      $scope.random = ()=> {
         var rand = 1000 - 0.5 + Math.random() * (9999 - 1000 + 1)
         $scope.rand = Math.round(rand);
     }
-//RANDOM-end
+//RANDOM-end/////////////////////////////////////////////////////////////////
 
 
-//RELOAD-start
+//RELOAD-start/////////////////////////////////////////////////////////////////
     $scope.reload =()=> {
         location.reload()
     }
-//RELOAD-end
+//RELOAD-end/////////////////////////////////////////////////////////////////
 
   })  
